f0818d77963c764054c1e2a027baf7dbca1da880
AtharvaKadam27 <atharvak396@gmail.com>
2026-03-19 23:42:32 +0530

    Solve the issues of nodemailer

--- Perplexity/backend/src/controllers/auth.controller.js
+++ Perplexity/backend/src/controllers/auth.controller.js
@@ -1,20 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
 import userModel from "../models/user.model.js";
 import { sendEmail } from "../service/mail.service.js";
+import jwt from "jsonwebtoken";
 
 export async function register(req, res) {
   const { username, email, password } = req.body;
 
-  const isAlreadyRegistered = await userModel.findOne({
+  const isUserAlreadyExists = await userModel.findOne({
     $or: [{ email }, { username }],
   });
 
-  if (isAlreadyRegistered) {
-    return res
-      .status(400)
-      .json({ message: "User already registered", success: false });
+  if (isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "User with this email or username already exists",
+      success: false,
+      err: "User already exists",
+    });
   }
 
   const user = await userModel.create({ username, email, password });
+
+  const emailVerificationToken = jwt.sign(
+    {
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  console.log(
+    process.env.JWT_SECRET,
+    process.env.GOOGLE_USER,
+    process.env.GOOGLE_PASS,
+  );
 
   await sendEmail({
     to: email,
@@ -22,6 +40,9 @@ export async function register(req, res) {
     html: `
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
+                <p>Please verify your email address by clicking the link below:</p>
+                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `,
   });
